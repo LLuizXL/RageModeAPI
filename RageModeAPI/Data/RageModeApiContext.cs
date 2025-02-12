5e94f2891f4c500809c60c116cbee7f4f8d8a81c
Joaquim <jocaq124@gmail.com>
2025-02-12 14:22:00 -0300

    Adicionado String de conexão, e RageModeApiContext. E adicionao cors e JWT ao program.cs

---
+++ RageModeAPI/Data/RageModeApiContext.cs
@@ -0,0 +1,24 @@
+﻿using RageModeAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RageModeAPI.Data
+{
+    public class RageModeApiContext : IdentityDbContext
+    {
+        //Método construtor
+        public RageModeApiContext(DbContextOptions<RageModeApiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Personagem> Personagens { get; set; }
+        public DbSet<TipoPersonagem> TiposPersonagens { get; set; }
+        //Sobrescrever o método OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Personagem>().ToTable("Personagens");
+            modelBuilder.Entity<TipoPersonagem>().ToTable("TiposPersonagens");
+        }
+    }
+}
--- RageModeAPI/Models/Personagem.cs
+++ RageModeAPI/Models/Personagem.cs
@@ -2,5 +2,7 @@
 {
     public class Personagem
     {
+        public Guid PersonagemId { get; set; }
+        public string Nome { get; set; }
     }
 }
