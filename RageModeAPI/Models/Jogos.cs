a74f32389b39a83f892bc5e06283a20cac695c79
Luiz G. <luiz.mwlo76@gmail.com>
2025-02-12 14:26:06 -0300

    Adição de mais Models e relacionando-as

---
+++ RageModeAPI/Models/Jogos.cs
@@ -0,0 +1,10 @@
+﻿namespace RageModeAPI.Models
+{
+    public class Jogos
+    {
+        public Guid JogoId { get; set; }
+        public string JogoNome { get; set; }
+        public string JogoDescricao { get; set; }
+        public DateOnly AnoLancamento { get; set; }
+    }
+}
