8be8effc2e91e8204962927daf5924c64aecb582
Joaquim <jocaq124@gmail.com>
2025-02-12 16:08:25 -0300

    Alterado Likes para fazer o relacionamento(Tanto a Model, quanto no context)

--- RageModeAPI/Models/Post.cs
+++ RageModeAPI/Models/Post.cs
@@ -1,4 +1,6 @@
-﻿namespace RageModeAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RageModeAPI.Models
 {
     public class Post
     {
@@ -18,6 +20,14 @@
         public Jogos? Jogo { get; set; }
         public Guid PersonagemId { get; set; }
         public Personagem? Personagem { get; set; }
+        public ICollection<Likes> Likes { get; set; }
+        public ICollection<Comentarios> Comentarios { get; set; }
+
+        [NotMapped]
+        public int LikeCount => Likes?.Count ?? 0; // Calcula o número de likes
+
+        [NotMapped]
+        public int CommentCount => Comentarios?.Count ?? 0; // Calcula o número de comentários
 
 
     }
