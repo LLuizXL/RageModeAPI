9171d67a9f1d14ab2a29fdafdc4f11ae491a9ad5
limazxz <goubulsegue@gmail.com>
2025-02-18 08:18:32 -0300

    Corrigindo erros

---
+++ RageModeAPI/Controllers/PersonagensController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RageModeAPI.Data;
+using RageModeAPI.Models;
+
+namespace RageModeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonagensController : ControllerBase
+    {
+        private readonly RageModeApiContext _context;
+
+        public PersonagensController(RageModeApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Personagens
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Personagem>>> GetPersonagens()
+        {
+            return await _context.Personagens.ToListAsync();
+        }
+
+        // GET: api/Personagens/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personagem>> GetPersonagem(Guid id)
+        {
+            var personagem = await _context.Personagens.FindAsync(id);
+
+            if (personagem == null)
+            {
+                return NotFound();
+            }
+
+            return personagem;
+        }
+
+        // PUT: api/Personagens/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPersonagem(Guid id, Personagem personagem)
+        {
+            if (id != personagem.PersonagemId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(personagem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonagemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Personagens
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Personagem>> PostPersonagem(Personagem personagem)
+        {
+            _context.Personagens.Add(personagem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPersonagem", new { id = personagem.PersonagemId }, personagem);
+
+
+        }
+
+        // DELETE: api/Personagens/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePersonagem(Guid id)
+        {
+            var personagem = await _context.Personagens.FindAsync(id);
+            if (personagem == null)
+            {
+                return NotFound();
+            }
+
+            _context.Personagens.Remove(personagem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PersonagemExists(Guid id)
+        {
+            return _context.Personagens.Any(e => e.PersonagemId == id);
+        }
+    }
+}
